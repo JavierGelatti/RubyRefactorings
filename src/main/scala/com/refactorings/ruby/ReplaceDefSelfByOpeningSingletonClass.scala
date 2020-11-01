9118e71f69fde11023032b40d722b551ecfa58c1
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-11-01 16:56:19 -0300

    Improve 'Replace def self by opening singleton class' refactoring

--- src/main/scala/com/refactorings/ruby/ReplaceDefSelfByOpeningSingletonClass.scala
+++ src/main/scala/com/refactorings/ruby/ReplaceDefSelfByOpeningSingletonClass.scala
@@ -4,53 +4,125 @@ import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.codeInspection.util.{IntentionFamilyName, IntentionName}
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
-import com.intellij.psi.{PsiElement, PsiFileFactory, PsiWhiteSpace}
-import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.util.{PsiTreeUtil, PsiUtil}
+import com.intellij.psi.{PsiElement, PsiFileFactory}
 import com.refactorings.ruby.ReplaceDefSelfByOpeningSingletonClass.optionDescription
+import org.jetbrains.plugins.ruby.ruby.lang.RubyFileType
 import org.jetbrains.plugins.ruby.ruby.lang.psi.controlStructures.classes.RObjectClass
 import org.jetbrains.plugins.ruby.ruby.lang.psi.controlStructures.methods.{RMethod, RSingletonMethod}
-import org.jetbrains.plugins.ruby.ruby.lang.{RubyFileType, RubyLanguage}
+import org.jetbrains.plugins.ruby.ruby.lang.psi.expressions.RGroupedExpression
+
+import scala.annotation.tailrec
+import scala.reflect.ClassTag
 
 class ReplaceDefSelfByOpeningSingletonClass extends PsiElementBaseIntentionAction {
   @IntentionName override def getText: String = optionDescription
 
   @IntentionFamilyName override def getFamilyName = "Replace def self by opening singleton class"
 
-  override def invoke(project: Project, editor: Editor, elementPosta: PsiElement): Unit = {
-    val singletonMethod: RSingletonMethod = PsiTreeUtil.findFirstParent(
-      elementPosta,
-      (elemento: PsiElement) => {
-        elemento.isInstanceOf[RSingletonMethod]
-      }
-    ).asInstanceOf[RSingletonMethod]
+  override def invoke(project: Project, editor: Editor, element: PsiElement): Unit = {
+    implicit val currentProject: Project = project
 
-    val openSingletonClassTemplate = getPsiElement(project,
-      """
-        |class PARENT
-        |  class << self
-        |    def NAME
-        |      BODY
-        |    end
-        |  end
-        |end
-      """.trim.stripMargin).getFirstChild
+    val singletonMethod: RSingletonMethod = singletonMethodEnclosing(element).get
 
-    val method = PsiTreeUtil.findChildOfType(openSingletonClassTemplate, classOf[RMethod])
-    method.setName(singletonMethod.getNameIdentifier.getText)
-    method.getCompoundStatement.replace(singletonMethod.getCompoundStatement)
-    val openSingletonClass = PsiTreeUtil.findChildOfType(openSingletonClassTemplate, classOf[RObjectClass])
+    val openSingletonClassTemplate = getPsiElement("""
+      |class << OBJECT
+      |  def NAME
+      |    BODY
+      |  end
+      |end
+    """.trim.stripMargin)
+    val method = findChild[RMethod](openSingletonClassTemplate).get
+    val openSingletonClass = findChild[RObjectClass](openSingletonClassTemplate).get
+
+    copyParametersAndBodyTo(source = singletonMethod, target = method)
+    openSingletonClass.getObject.replace(singletonMethod.getClassObject)
 
     singletonMethod.replace(openSingletonClass)
   }
 
-  private def getPsiElement(project: Project, code: String) = {
-    PsiFileFactory.getInstance(project)
-      .createFileFromText("DUMMY.rb", RubyFileType.RUBY, code)
+  private def copyParametersAndBodyTo
+    (source: RSingletonMethod, target: RMethod)
+    (implicit project: Project)
+  = {
+    target.setName(source.getNameIdentifier.getText)
+    target.getArgumentList.replace(source.getArgumentList)
+    if (hasParametersBetweenParentheses(source)) {
+      addParenthesesToParameters(target)
+    }
+    target.getCompoundStatement.replace(source.getCompoundStatement)
+  }
+
+  private def addParenthesesToParameters
+    (method: RMethod)
+    (implicit project: Project)
+  = {
+    val parentheses = getPsiElement[RGroupedExpression]("()")
+    val leftParen = parentheses.getFirstChild
+    val rightParen = parentheses.getLastChild
+    method.addBefore(leftParen, method.getArgumentList)
+    method.addAfter(rightParen, method.getArgumentList)
+  }
+
+  private def hasParametersBetweenParentheses(method: RMethod) = {
+    method.getArgumentList.getPrevSibling.textMatches("(")
+  }
+
+  private def getPsiElement[ElementType <: PsiElement]
+    (code: String)
+    (implicit project: Project, e: ElementType DefaultsTo PsiElement, tag: ClassTag[ElementType]):
+  ElementType = {
+    findChild[ElementType](
+      PsiFileFactory
+        .getInstance(project)
+        .createFileFromText("DUMMY.rb", RubyFileType.RUBY, code)
+    ).get
   }
 
   override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
-    true
+    singletonMethodEnclosing(element).isDefined
+  }
+
+  private def singletonMethodEnclosing(element: PsiElement) = {
+    findParent[RSingletonMethod](element, treeHeightLimit = 4)
+  }
+
+  @tailrec
+  private def findParent[T <: PsiElement]
+    (descendantElement: PsiElement, treeHeightLimit: Int = -1, matching: T => Boolean = (_: T) => true)
+    (implicit tag: ClassTag[T]):
+  Option[T] = {
+    if (treeHeightLimit == 0) return None
+
+    descendantElement.getParent match {
+      case null => None
+      case element: T if tag.runtimeClass.isInstance(element) && matching(element) => Some(element)
+      case otherElement => findParent[T](otherElement, treeHeightLimit - 1)
+    }
+  }
+
+  private def findChild[T <: PsiElement]
+    (ancestorElement: PsiElement, treeHeightLimit: Int = -1, matching: T => Boolean = (_: T) => true)
+    (implicit tag: ClassTag[T]):
+  Option[T] = {
+    if (treeHeightLimit == 0) return None
+
+    ancestorElement.getChildren
+      .find(
+        element => tag.runtimeClass.isInstance(element) && matching(element.asInstanceOf[T])
+      )
+      .map(_.asInstanceOf[T])
+      .orElse({
+        ancestorElement.getChildren
+          .view
+          .flatMap(directChild => findChild[T](directChild, treeHeightLimit - 1, matching))
+          .headOption
+    })
+  }
+
+  sealed class DefaultsTo[Provided, Default]
+  object DefaultsTo {
+    implicit def useDefault[Default]: DefaultsTo[Default, Default] = new DefaultsTo
+    implicit def useProvided[Provided, Default]: DefaultsTo[Provided, Default] = new DefaultsTo
   }
 }
 
--- src/test/scala/com/refactorings/ruby/TestReplaceDefSelfByOpeningSingletonClass.scala
+++ src/test/scala/com/refactorings/ruby/TestReplaceDefSelfByOpeningSingletonClass.scala
@@ -1,9 +1,12 @@
 package com.refactorings.ruby
 
+import java.util.Collections
+
 import scala.language.reflectiveCalls
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
 import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl
 import org.jetbrains.plugins.ruby.ruby.lang.RubyFileType
+import org.junit.Assert.assertEquals
 import org.junit.{After, Before, Test}
 
 class TestReplaceDefSelfByOpeningSingletonClass {
@@ -14,6 +17,10 @@ class TestReplaceDefSelfByOpeningSingletonClass {
     fixtureFactory.createCodeInsightFixture(fixture, tempDirTestFixture)
   }
 
+  type RefactoringDefinition = {
+    val optionDescription: String
+  }
+
   @Before
   def setupInsightFixture(): Unit = insightFixture.setUp()
 
@@ -21,7 +28,7 @@ class TestReplaceDefSelfByOpeningSingletonClass {
   def tearDownInsightFixture(): Unit = insightFixture.tearDown()
 
   @Test
-  def test01() = {
+  def opensSingletonClassReplacingSelfDefForMethodsWithNoParameters(): Unit = {
     loadFileWith(
       """
         |class X
@@ -45,6 +52,161 @@ class TestReplaceDefSelfByOpeningSingletonClass {
       """)
   }
 
+  @Test
+  def opensSingletonClassReplacingSelfDefForMethodsWithParameters(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def self<caret>.m1(x, *xs, &proc)
+        |    42
+        |  end
+        |end
+      """)
+
+    applyRefactor(ReplaceDefSelfByOpeningSingletonClass)
+
+    expectResultingCodeToBe(
+      """
+        |class X
+        |  class << self
+        |    def m1(x, *xs, &proc)
+        |      42
+        |    end
+        |  end
+        |end
+      """)
+  }
+
+  @Test
+  def preservesParenthesesOfOriginalMethodParameters(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def self<caret>.m1()
+        |    42
+        |  end
+        |end
+      """)
+
+    applyRefactor(ReplaceDefSelfByOpeningSingletonClass)
+
+    expectResultingCodeToBe(
+      """
+        |class X
+        |  class << self
+        |    def m1()
+        |      42
+        |    end
+        |  end
+        |end
+      """)
+  }
+
+  @Test
+  def isNotAvailableWhenTheMethodIsDefinedNormally(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def <caret>m1()
+        |    42
+        |  end
+        |end
+      """)
+
+    assertRefactorNotAvailable(ReplaceDefSelfByOpeningSingletonClass)
+  }
+
+  @Test
+  def isNotAvailableWhenTheMethodIsDefinedNormallyEvenWhenItIsInsideASingletonMethod(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def self.m1
+        |    def <caret>m1()
+        |      42
+        |    end
+        |  end
+        |end
+      """)
+
+    assertRefactorNotAvailable(ReplaceDefSelfByOpeningSingletonClass)
+  }
+
+  @Test
+  def isAvailableWhenTheCaretIsInsideTheMethodName(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def self.m<caret>1
+        |    42
+        |  end
+        |end
+      """)
+
+    applyRefactor(ReplaceDefSelfByOpeningSingletonClass)
+
+    expectResultingCodeToBe(
+      """
+        |class X
+        |  class << self
+        |    def m1
+        |      42
+        |    end
+        |  end
+        |end
+      """)
+  }
+
+  @Test
+  def isAvailableWhenTheCaretIsInsideTheObjectName(): Unit = {
+    loadFileWith(
+      """
+        |class X
+        |  def se<caret>lf.m1
+        |    42
+        |  end
+        |end
+      """)
+
+    applyRefactor(ReplaceDefSelfByOpeningSingletonClass)
+
+    expectResultingCodeToBe(
+      """
+        |class X
+        |  class << self
+        |    def m1
+        |      42
+        |    end
+        |  end
+        |end
+      """)
+  }
+
+  @Test
+  def correctlyReplacesTheObjectInWhichTheMethodIsDefined(): Unit = {
+    loadFileWith(
+      """
+        |object = Object.new
+        |
+        |def object<caret>.m1
+        |  42
+        |end
+      """)
+
+    applyRefactor(ReplaceDefSelfByOpeningSingletonClass)
+
+    expectResultingCodeToBe(
+      """
+        |object = Object.new
+        |
+        |class << object
+        |  def m1
+        |    42
+        |  end
+        |end
+      """)
+  }
+
   def loadFileWith(codeToLoad: String) = {
     insightFixture.configureByText(
       RubyFileType.RUBY,
@@ -52,9 +214,16 @@ class TestReplaceDefSelfByOpeningSingletonClass {
     )
   }
 
-  def applyRefactor(refactorToApply: {val optionDescription: String}): Unit = {
+  def applyRefactor(refactorToApply: RefactoringDefinition): Unit = {
     insightFixture.launchAction(
       insightFixture.findSingleIntention(refactorToApply.optionDescription)
+    )
+  }
+
+  def assertRefactorNotAvailable(refactorToApply: RefactoringDefinition): Unit = {
+    assertEquals(
+      Collections.emptyList,
+      insightFixture.filterAvailableIntentions(refactorToApply.optionDescription)
     )
   }
 
