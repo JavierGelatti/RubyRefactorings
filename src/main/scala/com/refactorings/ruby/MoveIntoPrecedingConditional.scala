4d7898e71d8214f3edf83de2e3a76933ab9015f4
Facundo Javier Gelatti <javiergelatti@gmail.com>
2021-01-07 02:24:47 -0300

    First implementation of MoveIntoPrecedingConditional

---
+++ src/main/scala/com/refactorings/ruby/MoveIntoPrecedingConditional.scala
@@ -0,0 +1,52 @@
+package com.refactorings.ruby
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.refactorings.ruby.psi.{IfOrUnlessStatement, IfOrUnlessStatementExtension, PsiElementExtension}
+import org.jetbrains.plugins.ruby.ruby.lang.psi.RPsiElement
+import org.jetbrains.plugins.ruby.ruby.lang.psi.controlStructures.blocks.RCompoundStatement
+
+import java.util
+import scala.language.reflectiveCalls
+
+class MoveIntoPrecedingConditional extends RefactoringIntention(MoveIntoPrecedingConditional) {
+  override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
+    elementsToRefactor(element).isDefined
+  }
+
+  override protected def invoke(editor: Editor, focusedElement: PsiElement)(implicit currentProject: Project): Unit = {
+    val (conditionalStatement, elseBlock, currentStatement) = elementsToRefactor(focusedElement).get
+    val thenBlock = conditionalStatement.getThenBlock
+    val elsifBlocks = conditionalStatement.getElsifBlocks.map(_.getBody)
+
+    thenBlock.add(currentStatement)
+    elsifBlocks.foreach(_.add(currentStatement))
+    elseBlock.add(currentStatement)
+
+    currentStatement.delete()
+  }
+
+  private def elementsToRefactor(focusedElement: PsiElement) = {
+    for {
+      container <- focusedElement.findParentOfType[RCompoundStatement]()
+      siblingStatements = container.getStatements
+      (conditionalStatement, currentStatement) <- pairwise(siblingStatements).collectFirst {
+        case (conditionalStatement: IfOrUnlessStatement @unchecked, currentStatement)
+          if currentStatement.contains(focusedElement) => (conditionalStatement, currentStatement)
+      }
+      elseBlock <- conditionalStatement.elseBlock
+    } yield (conditionalStatement, elseBlock.getBody, currentStatement)
+  }
+
+  private def pairwise(siblingStatements: util.List[RPsiElement]) = {
+    siblingStatements.zip(siblingStatements.drop(1))
+  }
+}
+
+object MoveIntoPrecedingConditional extends RefactoringIntentionCompanionObject {
+  override def familyName: String = "Move statement into conditional"
+
+  override def optionDescription: String = "Move into conditional above"
+}
+
+
---
+++ src/test/scala/com/refactorings/ruby/TestMoveIntoPrecedingConditional.scala
@@ -0,0 +1,86 @@
+package com.refactorings.ruby
+
+import org.junit.Test
+
+class TestMoveIntoPrecedingConditional extends RefactoringTestRunningInIde {
+  @Test
+  def movesAStatementIntoTheThenAndElseBranchesOfAConditionalBeforeIt(): Unit = {
+    loadRubyFileWith(
+      """
+        |if<caret> condition
+        |  a
+        |else
+        |  b
+        |end
+        |
+        |<caret>thing
+        |another_thing
+      """)
+
+    applyRefactor(MoveIntoPrecedingConditional)
+
+    expectResultingCodeToBe(
+      """
+        |if condition
+        |  a
+        |  thing
+        |else
+        |  b
+        |  thing
+        |end
+        |
+        |
+        |another_thing
+      """)
+  }
+
+  @Test
+  def cannotBeAppliedIfTheConditionalHasNoElseBranch(): Unit = {
+    loadRubyFileWith(
+      """
+        |if<caret> condition
+        |  a
+        |end
+        |
+        |<caret>thing
+      """)
+
+    assertRefactorNotAvailable(MoveIntoPrecedingConditional)
+  }
+
+  @Test
+  def movesAStatementIntoAllElsifBlocksOfAConditionalBeforeIt(): Unit = {
+    loadRubyFileWith(
+      """
+        |if<caret> condition
+        |  a
+        |elsif another_condition
+        |  b
+        |else
+        |  c
+        |end
+        |
+        |<caret>thing
+        |another_thing
+      """)
+
+    applyRefactor(MoveIntoPrecedingConditional)
+
+    expectResultingCodeToBe(
+      """
+        |if condition
+        |  a
+        |  thing
+        |elsif another_condition
+        |  b
+        |  thing
+        |else
+        |  c
+        |  thing
+        |end
+        |
+        |
+        |another_thing
+      """)
+  }
+}
