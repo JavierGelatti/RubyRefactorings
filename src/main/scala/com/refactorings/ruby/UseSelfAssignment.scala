1aaf9e13d13067c759635ee77f6d1504e5a65073
Facundo Javier Gelatti <javiergelatti@gmail.com>
2021-01-08 11:29:46 -0300

    First implementation of UseSelfAssignment

---
+++ src/main/scala/com/refactorings/ruby/UseSelfAssignment.scala
@@ -0,0 +1,45 @@
+package com.refactorings.ruby
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.{PsiElement, PsiManager}
+import com.refactorings.ruby.psi.{Parser, PsiElementExtension}
+import org.jetbrains.plugins.ruby.ruby.lang.psi.expressions.{RAssignmentExpression, RBinaryExpression, RSelfAssignmentExpression}
+
+import scala.PartialFunction.condOpt
+
+class UseSelfAssignment extends RefactoringIntention(UseSelfAssignment) {
+  override def isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean = {
+    elementsToRefactor(element)(project).isDefined
+  }
+
+  override protected def invoke(editor: Editor, focusedElement: PsiElement)(implicit currentProject: Project): Unit = {
+    val (assignment, binaryExpression) = elementsToRefactor(focusedElement).get
+    val operator = binaryExpression.getOperation.getText
+
+    val selfAssignment = Parser.parse(s"variable ${operator}= value").childOfType[RSelfAssignmentExpression]()
+    selfAssignment.getObject.replace(assignment.getObject)
+    selfAssignment.getValue.replace(binaryExpression.getRightOperand)
+
+    assignment.replace(selfAssignment)
+  }
+
+  private def elementsToRefactor(focusedElement: PsiElement)(implicit project: Project) = {
+    for {
+      assignment <- focusedElement.findParentOfType[RAssignmentExpression]()
+      if !assignment.isInstanceOf[RSelfAssignmentExpression]
+      binaryExpression <- condOpt(assignment.getValue) {
+        case binaryExpression: RBinaryExpression => binaryExpression
+      }
+      if binaryExpression.getLeftOperand.textMatches(assignment.getObject.getText) // TODO: consider whitespace differences
+    } yield (assignment, binaryExpression)
+  }
+}
+
+object UseSelfAssignment extends RefactoringIntentionCompanionObject {
+  override def familyName: String = "Replace binary operator assignment by self-assignment shorthand notation"
+
+  override def optionDescription: String = "Replace by self-assignment"
+}
+
+
---
+++ src/test/scala/com/refactorings/ruby/TestUseSelfAssignment.scala
@@ -0,0 +1,75 @@
+package com.refactorings.ruby
+
+import org.junit.Test
+
+class TestUseSelfAssignment extends RefactoringTestRunningInIde {
+  @Test
+  def replacesAssignmentOfBinaryMessageSendWithSelfAssignment(): Unit = {
+    loadRubyFileWith(
+      """
+        |a =<caret> a + 1
+      """)
+
+    applyRefactor(UseSelfAssignment)
+
+    expectResultingCodeToBe(
+      """
+        |a += 1
+      """)
+  }
+
+  @Test
+  def replacesAssignmentOfBinaryOperatorWithSelfAssignment(): Unit = {
+    loadRubyFileWith(
+      """
+        |a =<caret> a || 1
+      """)
+
+    applyRefactor(UseSelfAssignment)
+
+    expectResultingCodeToBe(
+      """
+        |a ||= 1
+      """)
+  }
+
+  @Test
+  def isNotAvailableIfTheCaretIsAlreadyInsideOfASelfAssignment(): Unit = {
+    loadRubyFileWith(
+      """
+        |a +=<caret> a + 1
+      """)
+
+    assertRefactorNotAvailable(UseSelfAssignment)
+  }
+
+  @Test
+  def isNotAvailableIfTheCaretIsInsideOfAMultiAssignment(): Unit = {
+    loadRubyFileWith(
+      """
+        |a, b =<caret> a + 1
+      """)
+
+    assertRefactorNotAvailable(UseSelfAssignment)
+  }
+
+  @Test
+  def isNotAvailableIfTheValueOfTheAssignmentValueIsNotTheResultOfABinaryOperation(): Unit = {
+    loadRubyFileWith(
+      """
+        |a =<caret> a.plus(1)
+      """)
+
+    assertRefactorNotAvailable(UseSelfAssignment)
+  }
+
+  @Test
+  def isNotAvailableIfTheLeftHandSideOfTheBinaryExpressionIsNotTheSameAsTheAssignmentTarget(): Unit = {
+    loadRubyFileWith(
+      """
+        |a = 1 + a
+      """)
+
+    assertRefactorNotAvailable(UseSelfAssignment)
+  }
+}
